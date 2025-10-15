fe12d1293d4036b732cdec778085fe77d11e3e49
Rajcode259 <rajbarkade259@gmail.com>
2025-10-15 20:25:07 +0530

    commited

---
+++ CEP_programs/inheritance.java
@@ -0,0 +1,18 @@
+package CEP_programs;
+class Animal{
+    void eat(){
+        System.out.println("Eating");
+    }
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Dog Barks");
+    }
+}
+public class inheritance{
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+    }
+}
